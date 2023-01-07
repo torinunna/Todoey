54742a99ea7806a6ef9d1e6113a36d9d400dfe54
torinunna <torinunna9588@gmail.com>
2023-01-07 23:51:33 +0900

    edit didSelectRowAt method to disappear checkmark

--- Todoey/ViewController.swift
+++ Todoey/ViewController.swift
@@ -45,8 +45,14 @@ extension ViewController: UITableViewDataSource {
 extension ViewController: UITableViewDelegate {
         
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
         tableView.deselectRow(at: indexPath, animated: true)
-        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
     }
 }
 
